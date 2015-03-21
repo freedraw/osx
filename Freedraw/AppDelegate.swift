c1139f41c2b59ed35b3148d0b138d0ee8d2ba040
Nathan Dinsmore <nfdins@gmail.com>
2015-03-21 18:15:26 -0400

    Initial Commit

---
+++ Freedraw/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Freedraw
+//
+//  Created by Nathan Dinsmore on 2015-03-21.
+//  Copyright (c) 2015 Nathan Dinsmore. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
