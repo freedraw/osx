a2fb1e69ffce97d8d9cee38f9bc808ad7a71d6ec
Nathan Dinsmore <nfdins@gmail.com>
2015-03-21 20:35:04 -0400

    Remove windowControllerDidLoadNib override
    Same goes for you.

--- Freedraw/Document.swift
+++ Freedraw/Document.swift
@@ -2,11 +2,6 @@ import Cocoa
 
 class Document: NSDocument {
 
-    override func windowControllerDidLoadNib(aController: NSWindowController) {
-        super.windowControllerDidLoadNib(aController)
-        // Add any code here that needs to be executed once the windowController has loaded the document's window.
-    }
-
     override class func autosavesInPlace() -> Bool {
         return true
     }
