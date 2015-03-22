d8469fe07d21e38c5596e6953f4e062099854573
Nathan Dinsmore <nfdins@gmail.com>
2015-03-21 20:50:07 -0400

    Remove applicationWillTerminate

--- Freedraw/AppDelegate.swift
+++ Freedraw/AppDelegate.swift
@@ -2,8 +2,4 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
 }
