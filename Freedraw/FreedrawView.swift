58001311fd0cd1c573dcbe2159d64821fd25c0d5
Nathan Dinsmore <nfdins@gmail.com>
2015-03-23 01:23:49 -0400

    Remove unnecessary argument

--- Freedraw/FreedrawView.swift
+++ Freedraw/FreedrawView.swift
@@ -11,6 +11,6 @@ extension FreedrawView {
 
     override func magnifyWithEvent(event: NSEvent) {
         let position = self.convertPoint(event.locationInWindow, fromView: nil)
-        document?.callHook("dispatchMagnify", withArguments: [position.x, frame.height - position.y, event.magnification], error: nil)
+        document?.callHook("dispatchMagnify", withArguments: [position.x, frame.height - position.y, event.magnification])
     }
 }
